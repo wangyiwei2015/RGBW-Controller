456b401accfd008321a210c4162b7498903073a9
wangyiwei2015 <1538685974@qq.com>
2020-11-28 19:32:01 +0800

    UI changes

--- RGBController/ViewController.swift
+++ RGBController/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var urlField: UITextField!
+    @IBOutlet weak var hyperView: UIView!
     @IBOutlet weak var whiteLED: UIView!
     @IBOutlet weak var redLED: UIView!
     @IBOutlet weak var greenLED: UIView!
@@ -20,25 +21,40 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var txtg: UILabel!
     @IBOutlet weak var txtb: UILabel!
     var labels: [UILabel] = []
+    @IBOutlet weak var addedLight: UIView!
     
     var urlRoot: URL!
     var timer: Timer?
+    var lightComp: [CGFloat] = [0,0,0,0] {
+        didSet {
+            addedLight.backgroundColor = UIColor(
+                red: lightComp[1]*0.8 + lightComp[0]*0.2,
+                green: lightComp[2]*0.8 + lightComp[0]*0.2,
+                blue: lightComp[3]*0.8 + lightComp[0]*0.2,
+                alpha: 1)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         urlField.delegate = self
         leds = [whiteLED,redLED,greenLED,blueLED]
-        for led in leds {
-            led.layer.cornerRadius = led.bounds.height / 2.5
-        }
         labels = [txtw,txtr,txtg,txtb]
         for label in labels {
             label.text = "0"
         }
+        for led in leds {
+            led.layer.cornerRadius = led.bounds.height / 5
+        }
+        addedLight.layer.cornerRadius = addedLight.bounds.height/2.3
+        //addedLight.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        addedLight.backgroundColor = .black
+        hyperView.transform = CGAffineTransform(rotationAngle: .pi / 4)
     }
 
     @IBAction func sliderChanged(_ sender: UISlider) {
-        leds[sender.tag].alpha = CGFloat(sender.value/10002*0.8+0.2)
+        leds[sender.tag].alpha = CGFloat(sender.value/255*0.8+0.2)
+        lightComp[sender.tag] = CGFloat(sender.value/255)
         labels[sender.tag].text = "\(Int(sender.value))"
     }
     

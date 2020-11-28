d32fbc0554a3b650f98d90c51594e90fc9e62096
wangyiwei2015 <1538685974@qq.com>
2020-11-28 22:12:36 +0800

    updates

--- RGBController/ViewController.swift
+++ RGBController/ViewController.swift
@@ -27,13 +27,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
     var timer: Timer?
     var lightComp: [CGFloat] = [0,0,0,0] {
         didSet {
+            let w = lightComp[0] > 0.05 ? lightComp[0] : 0.05
+            let r = lightComp[1]
+            let g = lightComp[2]
+            let b = lightComp[3]
+            let lumMax = w + (r+g+b) / 3
+            let lum = sqrt(CGFloat(1)-(2-lumMax)*(2-lumMax)*0.25)
+//            let l2 = lumMax * lumMax
+//            let l3 = l2 * lumMax
+//            let l4 = l3 * lumMax
+//            let lum = l4/16-l3/2+l2+0.25
+            let pMax = lightComp.dropFirst().max()! + w/3
+            let rp = (w/3 + r) / pMax * lum
+            let gp = (w/3 + g) / pMax * lum
+            let bp = (w/3 + b) / pMax * lum
             addedLight.backgroundColor = UIColor(
-                red: lightComp[1]*0.8 + lightComp[0]*0.2,
-                green: lightComp[2]*0.8 + lightComp[0]*0.2,
-                blue: lightComp[3]*0.8 + lightComp[0]*0.2,
+                red: rp, green: gp, blue: bp,
                 alpha: 1)
         }
     }
+    var lastComp: [CGFloat] = [0,0,0,0]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,12 +57,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
             label.text = "0"
         }
         for led in leds {
-            led.layer.cornerRadius = led.bounds.height / 5
+            led.layer.cornerRadius = led.bounds.height / 2.3
         }
-        addedLight.layer.cornerRadius = addedLight.bounds.height/2.3
-        //addedLight.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        addedLight.layer.cornerRadius = addedLight.bounds.height/2.33
+        addedLight.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
         addedLight.backgroundColor = .black
         hyperView.transform = CGAffineTransform(rotationAngle: .pi / 4)
+        lightComp = [0,0,0,0]
     }
 
     @IBAction func sliderChanged(_ sender: UISlider) {
@@ -59,13 +73,30 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        urlRoot = validateURL(textField.text ?? "http://192.168.1.1")
+        urlRoot = validateURL(textField.text ?? "http://192.168.1.1/")
         textField.resignFirstResponder()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {[self]_ in
+            if lastComp != lightComp {
+                lastComp = lightComp
+                setLED(UInt8(lightComp[0] * 255),
+                       UInt8(lightComp[1] * 255),
+                       UInt8(lightComp[2] * 255),
+                       UInt8(lightComp[3] * 255))
+            }
+        })
         return true
     }
     
     func validateURL(_ input: String) -> URL {
         URL(string: input)!
+    }
+    
+    func setLED(_ w: UInt8, _ r: UInt8, _ g: UInt8, _ b: UInt8) {
+        let value = UInt32(w) << 24
+                  + UInt32(r) << 16
+                  + UInt32(g) << 8
+                  + UInt32(b)
+        invoke("set?\(value)")
     }
     
     func invoke(_ cmd: String) {
